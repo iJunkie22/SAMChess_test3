f5d626f759e0994a6ba84f826af5bd256bebf350
Ethan Randall <iJunkie22@gmail.com>
2016-02-19 16:11:11 -0500

    Initial commit

---
+++ Intermediate/Build/Mac/UE4Editor/Development/SAMChess_test3/SAMChess_test3.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_SAMCHESS_TEST3_H__
+#define __AUTO_SAMCHESS_TEST3_H__
+//Last Write: 2/19/2016 6:58:23 PM +00:00
+#include "/Users/ethan/Developer/Unreal/SAMChess_test3/Source/SAMChess_test3/SAMChess_test3.h"
+#endif//__AUTO_SAMCHESS_TEST3_H__
---
+++ Source/SAMChess_test3/SAMChess_test3GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SAMChess_test3GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMCHESS_TEST3_API ASAMChess_test3GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
