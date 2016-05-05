3db329202df91bf85c22858f5229a62ffddf652e
Ethan Randall <iJunkie22@gmail.com>
2016-05-05 09:17:52 -0400

    clean up repo

---
+++ Source/SAMChess_test3/SmartTile.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SAMChess_test3.h"
+#include "SmartTile.h"
+
+
+// Sets default values
+ASmartTile::ASmartTile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASmartTile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASmartTile::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
