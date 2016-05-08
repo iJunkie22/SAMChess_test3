f9b41e7acbfd92bce70c1a6a94996bface5ae120
Ethan Randall <iJunkie22@gmail.com>
2016-05-08 18:00:26 -0400

    Added experimental support for touch screens

---
+++ Source/SAMChess_test3/Private/MyCustomPlayerController.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SAMChess_test3.h"
+#include "MyCustomPlayerController.h"
+
+
+
+
---
+++ Source/SAMChess_test3/Public/MyCustomPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "MyCustomPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMCHESS_TEST3_API AMyCustomPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
